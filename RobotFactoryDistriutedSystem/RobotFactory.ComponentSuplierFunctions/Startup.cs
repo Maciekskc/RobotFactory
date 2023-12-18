6c0a4544dfe0e7b095b612eeb65c7654a65bbd8e
Maciej Białkowski <maciekskc@gmail.com>
2023-12-18 21:27:53 +0100

    fix problem of deserializing orchestrated function output

--- RobotFactoryDistriutedSystem/RobotFactory.ComponentSuplierFunctions/Startup.cs
+++ RobotFactoryDistriutedSystem/RobotFactory.ComponentSuplierFunctions/Startup.cs
@@ -1,13 +1,28 @@
 ﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
 
+[assembly: FunctionsStartup(typeof(RobotFactory.ComponentSupplier.Startup))]
 namespace RobotFactory.ComponentSupplier
 {
     public class Startup : FunctionsStartup
     {
         public override void Configure(IFunctionsHostBuilder builder)
         {
-            builder.Services.AddHttpClient();
+            builder.Services.AddHttpClient(); builder.Services.AddSingleton<IMessageSerializerSettingsFactory, CustomMessageSerializerSettingsFactory>();
+            builder.Services.AddSingleton<IMessageSerializerSettingsFactory, CustomMessageSerializerSettingsFactory>();
+        }
+
+        internal class CustomMessageSerializerSettingsFactory : IMessageSerializerSettingsFactory
+        {
+            public JsonSerializerSettings CreateJsonSerializerSettings()
+            {
+                return  new JsonSerializerSettings
+                {
+                    TypeNameHandling = TypeNameHandling.All
+                };
+            }
         }
     }
 }

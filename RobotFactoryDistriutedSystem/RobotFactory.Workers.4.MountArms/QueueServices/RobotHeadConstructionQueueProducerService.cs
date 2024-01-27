7cbad3586f7dd3fecc228ca92091050606025e0a
Maciej Białkowski <macb@netcompany.com>
2024-01-27 23:04:57 +0100

    Initialize project for rest of robot construction services

---
+++ RobotFactoryDistriutedSystem/RobotFactory.Workers.4.MountArms/QueueServices/RobotHeadConstructionQueueProducerService.cs
@@ -0,0 +1,11 @@
+﻿using RobotFactory.DataAccessLayer.QueueServices.BaseModels;
+
+namespace RobotFactory.Workers._4.MountArms.QueueServices
+{
+    public class RobotLegsConstructionQueueProducerService : BaseWorkerQueuePublisher
+    {
+        public RobotLegsConstructionQueueProducerService(IConfiguration configuration, ILogger<BaseWorkerQueuePublisher> logger) : base(configuration, logger)
+        {
+        }
+    }
+}
---
+++ RobotFactoryDistriutedSystem/RobotFactory.Workers.5.MountLegs/Program.cs
@@ -0,0 +1,17 @@
+using RobotFactory.DataAccessLayer.QueueServices.Interfaces.BaseModels;
+using RobotFactory.DataAccessLayer.Repositories.Interfaces;
+using RobotFactory.DataAccessLayer.Repositories;
+using RobotFactory.SharedComponents.Dtos.QueueObjects;
+using RobotFactory.Workers._2.MountLegsWorker.QueueServices;
+using RobotFactory.Workers._5.MountLegs;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services
+    .AddSingleton<IBaseWorkerQueueConsumer<RobotConstructionMountLegsMessage>,
+        RobotLegsConstructionQueueConsumerService>();
+builder.Services.AddSingleton<IRobotComponentsRepository, RobotComponentsRepository>();
+builder.Services.AddSingleton<IRobotRepository, RobotRepository>();
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();

9475b5b09d978a4f0047d05b2dfb7ed7028dae06
Maciej Białkowski <maciekskc@gmail.com>
2024-02-08 21:25:25 +0100

    Adjust component organizer project configuratiuon to queue services restructurization

---
+++ RobotFactoryDistriutedSystem/RobotFactory.Workers.ConstructionOrganizers/Helpers/QueueServicesConfiguration.cs
@@ -0,0 +1,47 @@
+﻿namespace RobotFactory.Workers.ConstructionOrganizers.Helpers
+{
+    public class QueueServicesConfiguration
+    {
+        public string QueueServiceUri
+        {
+            get;
+            set;
+        }
+        public string StartConstructionQueueName
+        {
+            get;
+            set;
+        }
+
+        public string StartConstructionQueueSasToken
+        {
+            get;
+            set;
+        }
+
+        public string MountBodyQueueName {
+            get;
+            set;
+        }
+
+        public string MountBodyQueueSasToken
+        {
+            get;
+            set;
+        }
+
+        
+
+        public string FinalizeConstructionQueueName
+        {
+            get;
+            set;
+        }
+
+        public string FinalizeConstructionQueueSasToken
+        {
+            get;
+            set;
+        }
+    }
+}
--- RobotFactoryDistriutedSystem/RobotFactory.Workers.ConstructionOrganizers/Program.cs
+++ RobotFactoryDistriutedSystem/RobotFactory.Workers.ConstructionOrganizers/Program.cs
@@ -1,20 +1,48 @@
+using Microsoft.Extensions.Options;
+using RobotFactory.DataAccessLayer.QueueServices.BaseModels;
 using RobotFactory.DataAccessLayer.QueueServices.Interfaces.BaseModels;
-using RobotFactory.DataAccessLayer.Repositories;
 using RobotFactory.DataAccessLayer.Repositories.Interfaces;
+using RobotFactory.DataAccessLayer.Repositories;
 using RobotFactory.SharedComponents.Dtos.QueueObjects;
-using RobotFactory.Workers.ConstructionOrganizer;
 using RobotFactory.Workers.ConstructionOrganizer.QueueServices;
 using RobotFactory.Workers.ConstructionOrganizers;
-
+using RobotFactory.Workers.ConstructionOrganizers.Helpers;
 
 var builder = Host.CreateApplicationBuilder(args);
-//builder.Services
-//    .AddSingleton<IBaseWorkerQueueConsumer<StartRobotConstructionMessage>,
-//        StartRobotConstructionQueueConsumerService>();
-//builder.Services.AddSingleton<IBaseWorkerQueuePublisher,RobotBodyConstructionQueueProducerService>();
-//builder.Services.AddSingleton<IRobotComponentsRepository, RobotComponentsRepository>();
-//builder.Services.AddSingleton<IRobotRepository, RobotRepository>();
 
+builder.Services.Configure<QueueServicesConfiguration>(builder.Configuration.GetSection("QueueServiceConfig"));
+
+//Register repositories
+builder.Services.AddSingleton<IRobotComponentsRepository, RobotComponentsRepository>();
+builder.Services.AddSingleton<IRobotRepository, RobotRepository>();
+
+//Register Services
+builder.Services.AddSingleton<IBaseWorkerQueueConsumer<StartRobotConstructionMessage>>(provider =>
+{
+    var config = provider.GetRequiredService<IOptions<QueueServicesConfiguration>>().Value;
+    var logger = provider.GetRequiredService<ILogger<BaseWorkerQueueConsumer<StartRobotConstructionMessage>>>();
+
+    return new StartRobotConstructionQueueConsumerService(
+        logger,
+        config.StartConstructionQueueName,
+        config.QueueServiceUri,
+        config.StartConstructionQueueSasToken
+    );
+});
+builder.Services.AddSingleton<IBaseWorkerQueuePublisher<RobotConstructionMountBodyMessage>>(provider =>
+{
+    var config = provider.GetRequiredService<IOptions<QueueServicesConfiguration>>().Value;
+    var logger = provider.GetRequiredService<ILogger<BaseWorkerQueuePublisher<RobotConstructionMountBodyMessage>>>();
+
+    return new BaseWorkerQueuePublisher<RobotConstructionMountBodyMessage>(
+        config.MountBodyQueueName,
+        config.QueueServiceUri,
+        config.MountBodyQueueSasToken,
+        logger);
+});
+
+//Register Workers
 builder.Services.AddHostedService<StartRobotConstructionWorker>();
+
 var host = builder.Build();
 host.Run();

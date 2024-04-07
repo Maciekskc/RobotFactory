d6bf92e3d16394b50e081d0489e3bdf312ee6707
Maciej Białkowski <macb@netcompany.com>
2024-04-07 21:56:00 +0200

    Synch changes from laptop. Script is still not fully functional

--- RobotFactoryDistriutedSystem/RobotFactoryWebApi/Infrastructure/Scripts/FetchStorageTokensAndUpdateApiConfiguration.ps1
+++ RobotFactoryDistriutedSystem/RobotFactoryWebApi/Infrastructure/Scripts/FetchStorageTokensAndUpdateApiConfiguration.ps1
@@ -5,7 +5,19 @@ param(
 
    [Parameter(Mandatory=$true)]
    [string]
-   $storageAccountName
+   $storageAccountName,
+
+   [Parameter(Mandatory=$false)]
+   [string]
+   $initializeRobotCreationQueueSasToken,
+
+   [Parameter(Mandatory=$false)]
+   [string]
+   $startRobotConstructionQueueSasToken,
+
+   [Parameter(Mandatory=$false)]
+   [string]
+   $kvName
 )
 
 # Check if user is logged in to Azure
@@ -16,14 +28,9 @@ if ($loginCheck) {
 }
 
 # Get connection string
-try {
-    $connectionString = az storage account show-connection-string --resource-group $resourceGroup --name $storageAccountName --key 'key2' --output tsv
-}
-catch {
-   Write-Error "An error occurred while getting the connection string. Please verify your resource group and storage account name."
-}
+$connectionString = az storage account show-connection-string --resource-group $resourceGroup --name $storageAccountName --key 'key2' --output tsv
 
-
+# Get queue Endpoint
 try {
     $queueEndpointUri = ''
     foreach($part in ($connectionString -split ';'))
@@ -37,5 +44,21 @@ try {
     Write-Output $queueEndpointUri
 }catch {
    Write-Error "An error occurred while getting queue endpoint. Connection string might be invalid."
-    
 }
+
+# Get queue sas tokens
+if($PSBoundParameters.ContainsKey('initializeRobotCreationQueueSasToken')){
+    $expiry = (Get-Date).AddHours(2).ToString("yyyy-MM-dd'T'HH:mm:ss'Z'")  # Set SAS token expiry to 2 hours from now
+    $sasToken = az storage queue generate-sas --name $initializeRobotCreationQueueSasToken --permissions rw --expiry $expiry --connection-string $connectionString --output tsv
+    Write-Output "SAS Token: $sasToken"
+}else{
+    Write-Output "'initializeRobotCreationQueueSasToken' parameter was not provided so sas token will not be generated."
+}
+
+
+
+if(!$PSBoundParameters.ContainsKey($kvName)){
+    Write-Output 'Skip writing to KeyVault as kv name is not provided'
+    return
+}
+

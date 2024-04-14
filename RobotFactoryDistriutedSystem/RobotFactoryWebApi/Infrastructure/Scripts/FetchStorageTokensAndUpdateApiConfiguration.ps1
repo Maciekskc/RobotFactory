98741766959fbec88b4197d2d2e0ab422fb86e0e
Maciej Białkowski <macb@netcompany.com>
2024-04-14 18:24:41 +0200

    Fix the scirpt to generate actual sas tokens for both queues and add scetions to save it in the KV. Scriptis almost done but requre double check of SAS encoding

--- RobotFactoryDistriutedSystem/RobotFactoryWebApi/Infrastructure/Scripts/FetchStorageTokensAndUpdateApiConfiguration.ps1
+++ RobotFactoryDistriutedSystem/RobotFactoryWebApi/Infrastructure/Scripts/FetchStorageTokensAndUpdateApiConfiguration.ps1
@@ -13,7 +13,7 @@ param(
 
    [Parameter(Mandatory=$false)]
    [string]
-   $startRobotConstructionQueueSasToken,
+   $startRobotConstructionQueueName,
 
    [Parameter(Mandatory=$false)]
    [string]
@@ -46,19 +46,37 @@ try {
    Write-Error "An error occurred while getting queue endpoint. Connection string might be invalid."
 }
 
-# Get queue sas tokens (Requires `z extension add --name storage-preview`)
+# Get queue sas tokens
+$expiry = (Get-Date).AddHours(2).ToString("yyyy-MM-dd'T'HH:mm:ss'Z'")  # Set SAS token expiry to 2 hours from now
+
 if($PSBoundParameters.ContainsKey('initializeRobotCreationQueueName')){
-    $expiry = (Get-Date).AddHours(2).ToString("yyyy-MM-dd'T'HH:mm:ss'Z'")  # Set SAS token expiry to 2 hours from now
-    $sasToken = az storage queue generate-sas --name $initializeRobotCreationQueueName --permissions rw --expiry $expiry --connection-string $connectionString --output tsv
-    Write-Output "SAS Token: $sasToken"
+    $initializeRobotCreationQueueSasToken = az storage queue generate-sas --name $initializeRobotCreationQueueName --permissions ru --expiry $expiry --connection-string $connectionString --output tsv
+    Write-Output "$initializeRobotCreationQueueName SAS Token: $initializeRobotCreationQueueSasToken"
 }else{
     Write-Output "'initializeRobotCreationQueueName' parameter was not provided so sas token will not be generated."
 }
 
+if($PSBoundParameters.ContainsKey('startRobotConstructionQueueName')){
+    $startRobotConstructionQueueSasToken = az storage queue generate-sas --name $startRobotConstructionQueueName --permissions ru --expiry $expiry --connection-string $connectionString --output tsv
+    Write-Output "$startRobotConstructionQueueName SAS Token: $startRobotConstructionQueueSasToken"
+}else{
+    Write-Output "'startRobotConstructionQueueName' parameter was not provided so sas token will not be generated."
+}
 
-
-if(!$PSBoundParameters.ContainsKey($kvName)){
+if(!$PSBoundParameters.ContainsKey('kvName')){
     Write-Output 'Skip writing to KeyVault as kv name is not provided'
     return
 }
 
+Write-Output 'Adding tokens to the KV'
+if ($initializeRobotCreationQueueSasToken -ne $null){
+    az keyvault secret set --name 'AzureStorageQueue--InitializeRobotCreationQueueSasToken' --vault-name $kvName --value "$initializeRobotCreationQueueSasToken"
+}else{
+    Write-Output "AzureStorageQueue--InitializeRobotCreationQueueSasToken was skipped since the value was not provided"
+}
+
+if ($startRobotConstructionQueueSasToken -ne $null){
+    az keyvault secret set --name 'AzureStorageQueue--StartRobotConstructionQueueSasToken' --vault-name $kvName --value "$startRobotConstructionQueueSasToken"
+}else{
+    Write-Output "AzureStorageQueue--StartRobotConstructionQueueSasToken was skipped since the value was not provided"
+}

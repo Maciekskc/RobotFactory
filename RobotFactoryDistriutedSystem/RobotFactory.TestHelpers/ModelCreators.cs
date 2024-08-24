a4acc470d0ae743adb401eeae64c2ba262d34976
Maciej Białkowski <maciekskc@gmail.com>
2024-08-24 12:37:30 +0200

    Implement Robot Model Generator and unit tests

--- RobotFactoryDistriutedSystem/RobotFactory.TestHelpers/ModelCreators.cs
+++ RobotFactoryDistriutedSystem/RobotFactory.TestHelpers/ModelCreators.cs
@@ -47,5 +47,45 @@ namespace RobotFactory.TestHelpers
             .RuleFor(o => o.CreatedAt, DateTime.UtcNow)
             .RuleFor(o => o.MountedAt, f => null)
             .RuleFor(o => o.CPUCoresNumber, f => 2 ^ f.Random.Number(1, 4));
+
+        public static Faker<Robot> RobotGenerator = new Faker<Robot>()
+            .StrictMode(false)
+            .WithRobotGeneratorOptions();
+
+        private static Faker<Robot> WithRobotGeneratorOptions(this Faker<Robot> faker)
+        {
+            var fakeHelper = new Faker();
+            RobotConstrucionStatus constructionStatus = fakeHelper.PickRandom<RobotConstrucionStatus>();
+
+            faker.RuleFor(o => o.Id, ObjectId.GenerateNewId().ToString());
+            faker.RuleFor(o => o.OrderedAt, f => f.Date.Recent(10).ToUniversalTime());
+
+            switch (constructionStatus)
+            {
+                case RobotConstrucionStatus.Initialized:
+                    break;
+
+                case RobotConstrucionStatus.AwaitingComponents:
+                    break;
+
+                case RobotConstrucionStatus.ConstructionStarted:
+                    faker.RuleFor(o => o.Body, f => BodyComponentGenerator.Generate());
+                    break;
+
+                case RobotConstrucionStatus.Constructed:
+                    faker.RuleFor(o => o.Id, ObjectId.GenerateNewId().ToString());
+                    faker.RuleFor(o => o.Head,
+                        f => HeadComponentGenerator.Generate());
+                    faker.RuleFor(o => o.Body, f => BodyComponentGenerator.Generate());
+                    faker.RuleFor(o => o.Arms, (f, o) => ArmComponentGenerator.Generate(o.Body.ArmsNumbers));
+                    faker.RuleFor(o => o.Legs, (f, o) => LegComponentGenerator.Generate(o.Body.LegsNumber));
+                    faker.RuleFor(o => o.FinalizedAt, DateTime.UtcNow);
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+
+            return faker;
+        }
     }
 }
